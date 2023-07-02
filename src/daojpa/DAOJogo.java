2529901f42f847d97ae37874dc3afd26b4a1308d
Ricardo França <ricardofs547@live.com>
2023-07-01 23:33:17 -0300

    DAOjogo modificado para jpa

--- src/daojpa/DAOJogo.java
+++ src/daojpa/DAOJogo.java
@@ -6,48 +6,47 @@ import java.util.ArrayList;
 
 import com.db4o.query.Query;
 
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import modelo.Jogo;
 
 public class DAOJogo extends DAO<Jogo>{
 
 	public Jogo read (Object chave){
-
-		int id = (Integer) chave;	//casting para o tipo da chave
-		Query q = manager.query();
-		q.constrain(Jogo.class);
-		q.descend("id").constrain(id);
-		List<Jogo> resultados = q.execute();
-		if (resultados.size()>0)
-			return resultados.get(0);
-		else
-			return null;
-	}
-
+    try {
+		  int id = (Integer) chave;	//casting para o tipo da chave
+      TypedQuery<Jogo> q = manager.createQuery("select j from Jogo j where j.id = :i", Jogo.class);
+      q.setParameter("i", id);
+      Jogo jogo = q.getSingleResult();
+      return jogo;
+    } catch (NoResultException e) {
+      return null;      
+    }
 	//--------------------------------------------
 	//  consultas
 	//--------------------------------------------
 
-	public List<Jogo> listarJogos(){
-		Query q = manager.query();
-		q.constrain(Jogo.class);
-		return q.execute();
-	}
-
-	public List<Jogo> listarJogos(String data){
-		Query q = manager.query();
-		q.constrain(Jogo.class);
-		q.descend("data").constrain(data);
-		return q.execute();
-	}
-	
-	//	Consulta 1: Quais as datas que time x joga
-	
-	public List<Jogo> dataTimeJoga(String nomeTime){
-		Query q = manager.query();
-		q.constrain(Jogo.class);
-		q.descend("time1").descend("nome").constrain(nomeTime).or(q.descend("time2").descend("nome").constrain(nomeTime));
-		List<Jogo> resultado = q.execute();
-		return resultado;
+//	public List<Jogo> listarJogos(){
+//		Query q = manager.query();
+//		q.constrain(Jogo.class);
+//		return q.execute();
+//	}
+//
+//	public List<Jogo> listarJogos(String data){
+//		Query q = manager.query();
+//		q.constrain(Jogo.class);
+//		q.descend("data").constrain(data);
+//		return q.execute();
+//	}
+//	
+//	//	Consulta 1: Quais as datas que time x joga
+//	
+//	public List<Jogo> dataTimeJoga(String nomeTime){
+//		Query q = manager.query();
+//		q.constrain(Jogo.class);
+//		q.descend("time1").descend("nome").constrain(nomeTime).or(q.descend("time2").descend("nome").constrain(nomeTime));
+//		List<Jogo> resultado = q.execute();
+//		return resultado;
 	}
 }
 
